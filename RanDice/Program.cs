8300707b3d9967cdd97273b073ccd5097d6cbfa4
duducg <edu.sang.rocha@gmail.com>
2024-03-20 17:28:48 +0000

    Finished RanDice

--- RanDice/Program.cs
+++ RanDice/Program.cs
@@ -13,6 +13,18 @@ namespace RanDice
             //seed
             int s = int.Parse(args[1]);
             
+
+            //random
+            int total = 0;
+            Random r1 = new Random(s);
+            for(int i = 0; i < n; i++)
+            {
+                                
+                total += r1.Next(1,7); // maxvalue -1
+            }
+
+            //print total
+            Console.WriteLine(total);
             
             
         }
