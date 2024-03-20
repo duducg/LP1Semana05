855335a17310ececfa63445208f1cd96e0bb4144
duducg <edu.sang.rocha@gmail.com>
2024-03-20 19:14:24 +0000

    Added 2 new methods

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -19,5 +19,28 @@ namespace MyGame
             health = 100;
             shield = 0;
         }
+        //returns "name"
+        public string GetName()
+        {
+            return name;
+        }
+        //Deal damage to enemy
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            //if damage > shield then shield will total negative numb
+            if (shield < 0)
+            {
+                //this block only runs if the damage > shield
+                //since shield is negative we have to multiply it by -1 first.
+                float damageStillToInflict = -shield;
+                //we used the above var to capture the excess damage.
+                //We then reset the shield to 0. (Shield values can't be < 0)
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0; //if health goes below 0. reset it 0
+                
+            }
+        }
     }
 }
