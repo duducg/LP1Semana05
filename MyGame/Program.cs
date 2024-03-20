39ea138d9d393f632782f67a20afb5069080ae9c
duducg <edu.sang.rocha@gmail.com>
2024-03-20 19:39:39 +0000

    Added the array functionality

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,29 @@ namespace MyGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //use console input to get Enemy number:
+            int n = int.Parse(args[0]);
+
+            //Create an array with "n" number of indexes:
+            Enemy[] enemies = new Enemy[n];
+            
+            for (int i = 0; i < n; i++)
+            {
+                //prompt user for name
+                Console.Write($"Nome do inimigo {i + 1}: ");
+                string user_in = Console.ReadLine();
+                enemies[i] = new Enemy(user_in);
+            }
+            //after with we do another loop to print the array elements
+            foreach(Enemy enemy in enemies)
+            {
+                string name = enemy.GetName();
+                float health = enemy.GetHealth();
+                float shield = enemy.GetShield();
+
+                string output = $"{name} {health} {shield}";
+                Console.WriteLine(output);
+            }
         }
     }
 }
